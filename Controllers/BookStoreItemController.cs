8887096877276d380d202ef2a919f3e6c26b157d
Aliaksandr_Rymasheuski <Aliaksandr_Rymasheuski1@epam.com>
2023-08-24 16:08:31 +0300

    commit

--- Controllers/BookStoreItemController.cs
+++ Controllers/BookStoreItemController.cs
@@ -40,6 +40,7 @@ using BookStore.Enums;
             var items = _bookStoreService.BookStoreItems().Select(item => item).ToList();
             foreach(var item in items)
             {
+                // new comment
                var bookings = _bookStoreService.ProductAsIQ().Where(x => x.Id == item.ProductId).Select(x => x.Bookings).ToList();
                var bookedCount =  bookings.SelectMany(x => x).Where(x => x.StatusId == (int)Statuses.SUBMITED || x.StatusId == (int)Statuses.APPROVED).Count();
                var closedCount = bookings.SelectMany(x => x).Where(x => x.StatusId == (int)Statuses.COMPLETED).Count();
